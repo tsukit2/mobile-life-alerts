249966b2c479761e8e4b4b0cae8a87458450f537
chateluu <chateluu@547eac60-c646-0410-91ff-3da204c49a94>
2008-03-15 21:54:43 +0000

    Add ability to show addresses (physical) from Contacts app and populate the AppConfiguration when user selects one.

--- src/com/lifealert/activity/SelectContactInfoActivity.java
+++ src/com/lifealert/activity/SelectContactInfoActivity.java
@@ -2,7 +2,10 @@ package com.lifealert.activity;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import android.app.ListActivity;
 import android.database.Cursor;
@@ -11,7 +14,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.lifealert.EmergencyPersonInfo;
+import com.lifealert.ContactInfo;
 import com.lifealert.R;
 import com.lifealert.config.AppConfiguration;
 
@@ -46,7 +49,7 @@ public class SelectContactInfoActivity extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
         
         super.onListItemClick(l, v, position, id);
-        EmergencyPersonInfo personInfo = (EmergencyPersonInfo) nameIdHash.get(position);
+        ContactInfo personInfo = (ContactInfo) nameIdHash.get(position);
         
         // Store the selected contact info in the AppConfiguration
         if (ConfigurationActivity.USER_CONTACT_TYPE.equals(contactType)) {
@@ -54,17 +57,19 @@ public class SelectContactInfoActivity extends ListActivity {
         	AppConfiguration.setUserContactId(personInfo.getPersonId());
         	AppConfiguration.setUserName(personInfo.getName());
         	AppConfiguration.setUserPhone(personInfo.getPhoneNumber());
-        	AppConfiguration.setUserAddress(personInfo.getEmail());
+        	AppConfiguration.setUserEmail(personInfo.getEmail());
+        	AppConfiguration.setUserAddress(personInfo.getAddress());
         }
         else {
         	//Store Emergency Contact info
         	AppConfiguration.setEmergencyContactId(personInfo.getPersonId());
         	AppConfiguration.setEmergencyName(personInfo.getName());
         	AppConfiguration.setEmergencyPhone(personInfo.getPhoneNumber());
-        	AppConfiguration.setEmergencyAddress(personInfo.getEmail());
+        	AppConfiguration.setEmergencyEmail(personInfo.getEmail());
+        	AppConfiguration.setEmergencyAddress(personInfo.getAddress());
         }
         	   
-        setResult(RESULT_OK); //, null, bundle);
+        setResult(RESULT_OK);
         finish();      
         
     }	
@@ -94,7 +99,7 @@ public class SelectContactInfoActivity extends ListActivity {
     	
 		List<String> returnContacts = new ArrayList<String>();
     	Cursor managedPhoneCursor = null;
-    	Cursor managedEmailCursor = null;
+    	Cursor managedContactMethodsCursor = null;
     	
     	// Query the list of contacts phones	    
 		String[] phoneContactProjection = new String[] {
@@ -111,62 +116,96 @@ public class SelectContactInfoActivity extends ListActivity {
 		                        , android.provider.Contacts.PeopleColumns.NAME + " ASC"); // Order-by clause.
 
 		// Also query the list of emails tied to the same contact list
-		String[] emailContactProjection = new String[] {
+		String[] contactMethodsProjection = new String[] {
 		    android.provider.BaseColumns._ID
 		    , android.provider.Contacts.People._ID
-		    , android.provider.Contacts.ContactMethods.DATA
+		    , android.provider.Contacts.People.ContactMethods.KIND
+		    , android.provider.Contacts.People.ContactMethods.TYPE
+		    , android.provider.Contacts.People.ContactMethods.DATA
+		    , android.provider.Contacts.PeopleColumns.NAME
 		};
 		
-		managedEmailCursor = managedQuery(android.provider.Contacts.ContactMethods.CONTENT_URI
-				, emailContactProjection
-				, null //android.provider.Contacts.ContactMethods.PERSON_ID+"=\'"+Personid+"\'"
+		managedContactMethodsCursor = managedQuery(android.provider.Contacts.ContactMethods.CONTENT_URI
+				, contactMethodsProjection
+				, null //android.provider.Contacts.ContactMethods.PERSON_ID+"=\'"+Personid+"\'" /***/
 				, null); 
 
 		// Prepare the data columns
 		int idColumn = managedPhoneCursor.getColumnIndex(android.provider.BaseColumns._ID);
 	    int nameColumn = managedPhoneCursor.getColumnIndex(android.provider.Contacts.PeopleColumns.NAME); 
-	    int phoneColumn = managedPhoneCursor.getColumnIndex(android.provider.Contacts.PhonesColumns.NUMBER);
+	    int phoneNumberColumn = managedPhoneCursor.getColumnIndex(android.provider.Contacts.PhonesColumns.NUMBER);
+	    int phoneNumberTypeColumn = managedPhoneCursor.getColumnIndex(android.provider.Contacts.PhonesColumns.TYPE);
 	    int phonePersonIdColumn = managedPhoneCursor.getColumnIndex(android.provider.Contacts.People._ID);
-		int emailColumn = managedEmailCursor.getColumnIndex(android.provider.Contacts.ContactMethods.DATA);
-	    int emailPersonIdColumn = managedEmailCursor.getColumnIndex(android.provider.Contacts.People._ID);
+	    int contactMethodsPersonIdColumn = managedContactMethodsCursor.getColumnIndex(android.provider.Contacts.People._ID);
+	    int contactMethodsKindColumn = managedContactMethodsCursor.getColumnIndex(android.provider.Contacts.People.ContactMethods.KIND);
+		int contactMethodsTypeColumn = managedContactMethodsCursor.getColumnIndex(android.provider.Contacts.People.ContactMethods.TYPE);
+		int contactMethodsDataColumn = managedContactMethodsCursor.getColumnIndex(android.provider.Contacts.People.ContactMethods.DATA);
+		int contactMethodPeopleNameColumn = managedContactMethodsCursor.getColumnIndex(android.provider.Contacts.PeopleColumns.NAME);
+
+		/*************************************************************/
+		//Loop through to get the name and phone contact data first
+		ContactInfo contactInfo;
+		HashMap<String, ContactInfo> contactHash = new HashMap();
 		
-		//Store the email in a hash so that we can search for it based on the person ID
-	    HashMap emailList = new HashMap();
-        while (managedEmailCursor.next()) {
-	    	//If at least a contact, loop through and display to user
-        	emailList.put(managedEmailCursor.getLong(emailPersonIdColumn)
-        			    , managedEmailCursor.getString(emailColumn));
-    	    
-	    }
-	    
-		// We need a list of name, phone, and email for each of the contact items
-	    Long id;
-	    String name; 
-	    String phoneNumber;
-	    String emailAddress;
-	    Long phonePersonId;
-	    String contactDisplayFormat;
-	    EmergencyPersonInfo personInfo;
-	    while (managedPhoneCursor.next()) {
-	    	
-	    	//If at least a contact, loop through and display to user
-    	    id = managedPhoneCursor.getLong(idColumn);
-	    	name = managedPhoneCursor.getString(nameColumn);
-	        phoneNumber = managedPhoneCursor.getString(phoneColumn);
-	        phonePersonId = managedPhoneCursor.getLong(phonePersonIdColumn);
-	        emailAddress = (String) emailList.get(phonePersonId);
-	        contactDisplayFormat = name 
-	        				     + "\n\t" + phoneNumber 
-	        				     + "\n\t" + emailAddress;  
-	        
-	        // Add this contact so that it can be displayed
-	        returnContacts.add(contactDisplayFormat);
-	        
-	        // Add this contact to the hash
-	        personInfo = new EmergencyPersonInfo(id, name, phoneNumber, emailAddress);
-	        nameIdHash.put(returnContacts.indexOf(contactDisplayFormat), personInfo);    
-	    }
-	    
+		Long personId;
+		String name, phoneNumber, phoneNumberType;
+		while (managedPhoneCursor.next()) {
+			personId = managedPhoneCursor.getLong(phonePersonIdColumn);
+			name = managedPhoneCursor.getString(nameColumn);
+			phoneNumber = managedPhoneCursor.getString(phoneNumberColumn);
+			phoneNumberType = managedPhoneCursor.getString(phoneNumberTypeColumn);
+			
+			//Store the name and phone number into a hash first. We will get the associated email + address next
+			//Log.d("Preparing phone data = ", personId + ", " + name + ", " + phoneNumber + ", " + phoneNumberType);
+			contactInfo = new ContactInfo(personId, name, phoneNumber, phoneNumberType);
+			contactHash.put(name, contactInfo);
+		}
+		
+		//Loop through to get the email and address contact data next
+		int kind, type; 
+		String data;
+		while (managedContactMethodsCursor.next()) {
+			personId = managedContactMethodsCursor.getLong(contactMethodsPersonIdColumn);
+			kind = managedContactMethodsCursor.getInt(contactMethodsKindColumn);
+			type = managedContactMethodsCursor.getInt(contactMethodsTypeColumn);
+			data = managedContactMethodsCursor.getString(contactMethodsDataColumn);
+			name = managedContactMethodsCursor.getString(contactMethodPeopleNameColumn);
+			
+			//Log.d("Preparing contact methods data = ", personId + ", " + kind + ", " + type + ", " + data + ", " + name);
+			
+			//Get the stored contactInfo object with same personId
+			contactInfo = contactHash.get(name);
+			
+			if (kind == android.provider.Contacts.ContactMethods.EMAIL_KIND) {
+				//Store the email address and type
+				contactInfo.setEmailType(""+type);
+				contactInfo.setEmail(data);
+				contactHash.put(name, contactInfo);
+			}
+			else if (kind == android.provider.Contacts.ContactMethods.POSTAL_KIND) {
+				//Store the physical address and type
+				contactInfo.setAddressType(""+type);
+				contactInfo.setAddress(data);
+				contactHash.put(name, contactInfo);
+			}
+			else {
+				; //Hit a data kind that we don't care. Don't do anything, but log it incase.
+			}	
+		}
+		
+		// Sort the contacts, based on name (which is the key to the HashMap), ASC order
+		Map sortedMap = new TreeMap(contactHash);
+		
+		//Setup what should be displayed on the screen now
+		Iterator<ContactInfo> iter = sortedMap.values().iterator();
+		String displayString;
+		while (iter.hasNext()) {
+			contactInfo = iter.next();
+			displayString = contactInfo.toString();
+			returnContacts.add(displayString);
+			nameIdHash.put(returnContacts.indexOf(displayString), contactInfo);
+		}
+		
 	    return returnContacts;
     }
 }
