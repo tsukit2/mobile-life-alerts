bbd3f5c49088ae76394c9bb5ba2420dbbeeeee85
chateluu <chateluu@547eac60-c646-0410-91ff-3da204c49a94>
2008-04-04 06:25:48 +0000

    checkin what I have so far

--- trunk/src/com/lifealert/activity/SendEmailActivity.java
+++ trunk/src/com/lifealert/activity/SendEmailActivity.java
@@ -5,43 +5,66 @@ import com.lifealert.R;
 import com.lifealert.config.AppConfiguration;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.Window;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SendEmailActivity extends Activity {
-
+	
+	private TextView textView;
+	
 	@Override
 	protected void onCreate(Bundle icicle) {
 		super.onCreate(icicle);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.emailhelp);
-		Log.e("Life", "It's in the email activity");
-//		emailHandler.sendEmptyMessage(0);
+        
+		emailHandler.sendEmptyMessage(0);
+				
 	}
+
 	
-   private Handler emailHandler = new Handler() {
+    private Handler emailHandler = new Handler() {
       @Override
       public void handleMessage(Message msg) {
          //Get the emergency contact email address
          String emergencyEmail = AppConfiguration.getEmergencyEmail();
 
          if (emergencyEmail != null && !"".equals(emergencyEmail)) {
-            Toast.makeText(SendEmailActivity.this
-                  , "Sending an emergency email to the following email adress: " + emergencyEmail
-                  , Toast.LENGTH_SHORT).show();
-
+        	 
+        	// Request for the progress bar to be shown in the title
+        	setProgressBarVisibility(true);
+        	textView = (TextView) findViewById(R.id.email_help);
+            //textView.setAlignment(android.text.Layout.Alignment.ALIGN_CENTER);
+            //textView.setText(R.string.sending_email);
+            
             sendEmergencyEmail(emergencyEmail);
             
+            setProgressBarVisibility(false);
+	    	textView.setText(R.string.sent_email);
+	    	
+    		
+            //Move to the final screen
+		    //Intent intent = new Intent(getApplication(), AppCompleteActivity.class);
+		    //startActivity(intent);
+		    
+		    //finish();
          }
          else {
             //No emergency email set. Display message.
             Toast.makeText(SendEmailActivity.this
                   , "No emergency email set. No email was sent.", Toast.LENGTH_LONG).show();
          }
+        
       }
-   };
+    };
 	
 	
 	/**
@@ -49,12 +72,17 @@ public class SendEmailActivity extends Activity {
 	 * @param emergencyEmail
 	 */
 	private void sendEmergencyEmail(String emergencyEmail) {
+		
+		//ProgressDialog.show(SendEmailActivity.this,
+        //        "Send Emergency Email Message", "Please wait while sending email...", true, true);
+			
 		String subject = "EMERGENCY -- NEED HELP -- " + AppConfiguration.getUserName();
 		String body = AppConfiguration.getTextMsg();
 		String sender = AppConfiguration.getUserEmail();
 		String recipients = AppConfiguration.getEmergencyEmail();
 
-		GmailSender gmailSender = new GmailSender("to_change", "to_change");
+		//TODO: Remove hardcoded password of user's Gmail account
+		GmailSender gmailSender = new GmailSender(recipients, "moblielifealerts_01"); 
 		try {
 			gmailSender.sendMail(subject, body, sender, recipients);
 		}
@@ -62,5 +90,6 @@ public class SendEmailActivity extends Activity {
 			Toast.makeText(this, getClass().getName(), Toast.LENGTH_SHORT).show();
 			Log.e(getClass().getName(), ex.getMessage(), ex);
 		}
+
 	}	
 }
